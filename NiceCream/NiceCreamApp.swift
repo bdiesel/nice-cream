7b0807d1727eae0b73d75bd40a18d5a98cc5dbfb
Brian Diesel <bri@ndiesel.net>
2025-01-10 12:00:43 -0500

    Initial Commit

---
+++ NiceCream/NiceCreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiceCreamApp.swift
+//  NiceCream
+//
+//  Created by Brian Diesel on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct NiceCreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NiceCreamTests/NiceCreamTests.swift
@@ -0,0 +1,16 @@
+//
+//  NiceCreamTests.swift
+//  NiceCreamTests
+//
+//  Created by Brian Diesel on 1/10/25.
+//
+
+import Testing
+
+struct NiceCreamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
